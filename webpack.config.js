cfc40cc1564cc1f6a7d0161d6e07ece9386a49c8
Justin Mauldin <justin.mauldin7@gmail.com>
2019-03-23 21:05:19 -0600

    Adds webpack and gets bundle files to work.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: "./lib/index.js",
+    test: "./test/index.spec.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loaders: ["style-loader", "css-loader"] },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.css', '.scss']
+  }
+};
